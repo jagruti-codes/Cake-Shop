de9be698d285c5f614e9b56c02502e4ba1277f10
Hiral Jadav <hjadav320@rku.ac.in>
2026-05-04 22:30:17 +0530

    Add files via upload

---
+++ wwwroot/js/filter.js
@@ -0,0 +1,27 @@
+﻿<script> //filtter script
+
+    let buttons = document.querySelectorAll(".filter-btn");
+    let cakes = document.querySelectorAll(".cake-item");
+
+    buttons.forEach(btn => {
+
+        btn.addEventListener("click", () => {
+
+            let filter = btn.getAttribute("data-filter");
+
+            cakes.forEach(cake => {
+
+                if (filter === "all" || cake.classList.contains(filter)) {
+                    cake.style.display = "block";
+                }
+                else {
+                    cake.style.display = "none";
+                }
+
+            });
+
+        });
+
+    });
+
+</script>
