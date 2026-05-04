886394aae0d16713b6ff1ecd0ef18fb0b3510e11
Hiral Jadav <hjadav320@rku.ac.in>
2026-05-04 22:31:51 +0530

    Add files via upload

---
+++ wwwroot/js/wishlist.js
@@ -0,0 +1,15 @@
+﻿<script>
+
+    function addWishlist(name){
+
+        let wishlist = JSON.parse(localStorage.getItem("wishlist")) || [];
+
+    wishlist.push(name);
+
+    localStorage.setItem("wishlist",JSON.stringify(wishlist));
+
+    alert("Added to Wishlist");
+
+}
+
+</script>
